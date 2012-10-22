01457eeacf3125beb1569cb773f85b0229d34963
Alex Sanford <alex.sanford1@gmail.com>
2012-10-22 13:04:13 -0300

    Minimal save works for content_types and content_entries

--- app/presenters/locomotive/extensions/site_data_presenter/validation_and_save/minimal_save.rb
+++ app/presenters/locomotive/extensions/site_data_presenter/validation_and_save/minimal_save.rb
@@ -6,23 +6,29 @@ module Locomotive
 
           protected
 
+          def models_for_minimal_save
+            %w{pages content_types content_entries}
+          end
+
           def minimal_save_all
             all_valid = true
             without_callbacks_and_validations do
               _all_objects do |obj, model, *path|
-                without_extra_attributes(obj, model) do
-                  # TODO: validating...not DRY
-                  if model == 'content_entries' && !obj.content_type
-                    content_type_slug, index = *path
-                    all_valid = false
-                    set_errors('content type does not exist', model, content_type_slug)
-                  else
-                    if obj.valid?
-                      all_valid = obj.save && all_valid
-                    else
-                      all_valid = false
-                      set_errors(obj, model, *path)
-                    end
+                if models_for_minimal_save.include?(model)
+                  without_extra_attributes(obj, model) do
+                    # TODO: validating...not DRY
+                    #if model == 'content_entries' && !obj.content_type
+                      #content_type_slug, index = *path
+                      #all_valid = false
+                      #set_errors('content type does not exist', model, content_type_slug)
+                    #else
+                      if obj.valid?
+                        all_valid = obj.save && all_valid
+                      else
+                        all_valid = false
+                        set_errors(obj, model, *path)
+                      end
+                    #end
                   end
                 end
               end
@@ -37,25 +43,45 @@ module Locomotive
           end
 
           def should_skip_callback(model, callback)
-            if model == 'pages'
-              filter = callback.filter
-              raw_filter = callback.raw_filter
-              if raw_filter.kind_of?(::Mongoid::Validations::UniquenessValidator) && raw_filter.attributes.include?(:slug)
-                false
-              else
-                filters_to_keep = %w{normalize_slug}.map { |f| f.to_sym }
-                !filters_to_keep.include?(filter)
-              end
-            else
-              true
+            raw_filter = callback.raw_filter
+
+            # Always validate presence and uniqueness of slug
+            uniqueness_val = ::Mongoid::Validations::UniquenessValidator
+            presence_val = ::Mongoid::Validations::PresenceValidator
+            if (raw_filter.kind_of?(uniqueness_val) ||
+                raw_filter.kind_of?(presence_val)) \
+                && raw_filter.attributes.include?(:slug)
+              return false
             end
+
+            if model == 'pages'
+              if (raw_filter.kind_of?(presence_val)) \
+                  && raw_filter.attributes.include?(:title)
+                return false
+              end
+            elsif model == 'content_types'
+              if (raw_filter.kind_of?(presence_val)) \
+                  && raw_filter.attributes.include?(:name)
+                return false
+              end
+            end
+
+            filters_to_keep = Hash.new { |hash, key| hash[key] = [] }
+            filters_to_keep.merge!({
+              'pages' => %w{normalize_slug},
+              'content_types' => %w{normalize_slug},
+              'content_entries' => %w{set_slug}
+            })
+
+            filter = callback.filter
+            !filters_to_keep[model].include?(filter.to_s)
           end
 
           def without_callbacks
             callbacks = {}
 
             # Skip all callbacks
-            self.models.each do |model|
+            self.models_for_minimal_save.each do |model|
               klass = model_class(model)
 
               callbacks[model] = []
@@ -76,7 +102,7 @@ module Locomotive
             yield
 
             # Set all callbacks
-            self.models.each do |model|
+            self.models_for_minimal_save.each do |model|
               klass = model_class(model)
               callbacks[model].each do |callback|
                 klass.set_callback(callback.name, callback.kind,
@@ -86,24 +112,40 @@ module Locomotive
           end
           alias :without_callbacks_and_validations :without_callbacks
 
+          def attributes_to_keep(model)
+            default_attributes_to_keep = %w{_id _type slug site_id}
+            attributes_to_keep_for_model = Hash.new { |hash, key| hash[key] = [] }
+
+            attributes_to_keep_for_model.merge!({
+              'pages' => %w{title},
+              'content_types' => %w{name},
+              'content_entries' => %w{name content_type_id custom_fields_recipe}
+            })
+
+            default_attributes_to_keep + attributes_to_keep_for_model[model]
+          end
+
+          def removal_value(model, meth)
+            case model
+            when 'pages'
+              return [] if meth == 'parent_ids'
+            end
+
+            nil
+          end
+
           def without_extra_attributes(obj, model)
             attributes_removed = {}
-            if model == 'pages'
-              attributes_to_keep = %w{_id slug title site_id}
-              obj.attributes.keys.each do |attr|
-                next if attributes_to_keep.include?(attr)
 
-                meth = attr
-                new_meth = "#{attr}_translations"
-                meth = new_meth if obj.respond_to?(:"#{new_meth}")
+            obj.attributes.keys.each do |attr|
+              next if attributes_to_keep(model).include?(attr)
 
-                attributes_removed[meth] = obj.send(:"#{meth}")
-                if attr == 'parent_ids'
-                  obj.send(:"#{meth}=", [])
-                else
-                  obj.send(:"#{meth}=", nil)
-                end
-              end
+              meth = attr
+              new_meth = "#{attr}_translations"
+              meth = new_meth if obj.respond_to?(:"#{new_meth}")
+
+              attributes_removed[meth] = obj.send(:"#{meth}")
+              obj.send(:"#{meth}=", removal_value(model, meth))
             end
 
             yield
--- spec/presenters/extensions/site_data_presenter/validation_and_save/minimal_save.rb
+++ spec/presenters/extensions/site_data_presenter/validation_and_save/minimal_save.rb
@@ -12,7 +12,7 @@ module Locomotive
 
           context 'pages' do
 
-            it 'should only save page slugs and site_id' do
+            it 'should only save page slug, title and site_id' do
               params = {
                 :pages => [
                   new_page_attributes
@@ -24,13 +24,14 @@ module Locomotive
               page = site.pages.where(:slug => 'new-page').first
 
               page.slug.should == 'new-page'
-              page.raw_template.should_not == 'New page template'
+              page.title.should == 'New page'
+              page.raw_template.should_not == new_page_attributes[:raw_template]
             end
 
             it 'should generate the slug from the title' do
               params = {
                 :pages => [
-                  new_page_attributes_with_title
+                  new_page_attributes_without_slug
                 ]
               }.with_indifferent_access
               site_data.build_models(params)
@@ -65,20 +66,143 @@ module Locomotive
 
           end
 
+          context 'content_types' do
+
+            it 'should only save content_type slug, name and site_id' do
+              params = {
+                :content_types => [
+                  new_content_type_attributes
+                ]
+              }.with_indifferent_access
+              site_data.build_models(params)
+              site_data.send(:minimal_save_all).should be_true
+
+              content_type = site.content_types.where(:slug => 'projects').first
+
+              content_type.slug.should == 'projects'
+              content_type.name.should == 'Projects'
+              content_type.description.should_not == new_content_type_attributes[:description]
+            end
+
+            it 'should generate the slug from the name' do
+              params = {
+                :content_types => [
+                  new_content_type_attributes_without_slug
+                ]
+              }.with_indifferent_access
+              site_data.build_models(params)
+              site_data.send(:minimal_save_all).should be_true
+
+              content_type = site.content_types.where(:slug => 'projects').first
+
+              content_type.slug.should == 'projects'
+              content_type.name.should == 'Projects'
+            end
+
+            it 'should validate the uniqueness of the slug' do
+              params = {
+                :content_types => [
+                  new_content_type_attributes,
+                  new_content_type_attributes
+                ]
+              }.with_indifferent_access
+              site_data.build_models(params)
+              site_data.send(:minimal_save_all).should be_false
+
+              site_data.errors.should == {
+                'errors' => {
+                  'content_types' => {
+                    1 => {
+                      :slug => [ 'is already taken' ]
+                    }
+                  }
+                }
+              }
+            end
+
+          end
+
+          context 'content_entries' do
+
+            before(:each) do
+              create_projects_content_type!
+            end
+
+            it 'should only save content_entry slug, name and site_id' do
+              params = {
+                :content_entries => {
+                  :projects => [
+                    new_project_attributes
+                  ]
+                }
+              }.with_indifferent_access
+              site_data.build_models(params)
+              site_data.send(:minimal_save_all).should be_true
+
+              site.reload
+              content_type = site.content_types.where(:slug => 'projects').first
+              project = content_type.entries.where(:_slug => 'project-1').first
+
+              project._slug.should == 'project-1'
+              project.name.should == 'Project 1'
+              project.description.should_not == new_project_attributes[:description]
+            end
+
+            it 'should generate the slug from the label field'
+
+            it 'should validate the uniqueness of the slug'
+
+          end
+
           protected
+
+          ## Page data ##
 
           def new_page_attributes
             {
               :slug => 'new-page',
+              :title => 'New page',
               :raw_template => 'New page template'
             }
           end
 
-          def new_page_attributes_with_title
+          def new_page_attributes_without_slug
             attrs = new_page_attributes
             attrs.delete(:slug)
-            attrs[:title] = 'New page'
             attrs
+          end
+
+          ## Content type data ##
+
+          def new_content_type_attributes
+            {
+              :slug => 'projects',
+              :name => 'Projects',
+              :description => 'Projects that the company does'
+            }
+          end
+
+          def new_content_type_attributes_without_slug
+            attrs = new_content_type_attributes
+            attrs.delete(:slug)
+            attrs
+          end
+
+          ## Content entry data ##
+
+          def create_projects_content_type!
+            content_type = FactoryGirl.build(:content_type, :name => 'Projects')
+            content_type.entries_custom_fields.build(:label => 'Name', :type => 'string')
+            content_type.entries_custom_fields.build(:label => 'Description', :type => 'text')
+            content_type.save!
+          end
+
+          def new_project_attributes
+            {
+              :slug => 'project-1',
+              :name => 'Project 1',
+              :description => 'The first project ever'
+            }
           end
 
         end
